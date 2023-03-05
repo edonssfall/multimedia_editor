17ed3cb1b783ee7cb50ecb702a0cb758a1df453e
Leonid Tsarov <tsarov13leo@gmail.com>
2023-03-05 16:31:47 +0100

    v0.0.6_edit timing calculator

--- video_redactor.py
+++ video_redactor.py
@@ -20,7 +20,8 @@ class Video_redactor:
     def get_video_data(self):
         """
         Get videofile metadata: fps, fpms, resolution
-        and open file in cv2
+        Open file in cv2
+        Delete in string name format
         """
         self.video = cv2.VideoCapture(self.name)
         self.name = self.name[:self.name.rfind('.')]
@@ -55,7 +56,7 @@ class Video_redactor:
 
     def folder_frames(self):
         """
-        Make a folder with self.name and choose that folder to fold frames there
+        Make a folder with self.name and choose that folder to save frames there
         """
         folder_path = os.getcwd()
         try:
@@ -67,9 +68,17 @@ class Video_redactor:
         except OSError:
             print(f"You don't have permission to change or create")
 
+    def image_name(self, time_list):
+        name = f'{self.name}'\
+               f'_{str(time_list[0]).zfill(2)}'\
+               f':{str(time_list[1]).zfill(2)}'\
+               f':{str(time_list[2]).zfill(2)}'\
+               f':{str(time_list[3])[2:].zfill(3)}.jpg'
+        return name
+
     def split_video_to_frames(self):
         """
-        Split videofile to frames and call them in (self.name_HH:MM:SS:MSS)
+        Split videofile to frames, save and call them as (self.name_HH:MM:SS:MSS.jpg)
         """
         self.folder_frames()
         frames_counter = 1
@@ -79,21 +88,106 @@ class Video_redactor:
             if ret:
                 time_list = self.count_time(frames_counter, time_list)
                 cv2.imwrite(
-                    f'{self.name}'
-                    f'_{str(time_list[0]).zfill(2)}'
-                    f':{str(time_list[1]).zfill(2)}'
-                    f':{str(time_list[2]).zfill(2)}'
-                    f':{str(time_list[3])[2:].zfill(3)}.jpg',
+                    self.image_name(time_list),
                     frame
                 )
             else:
                 break
             frames_counter += 1
 
+    @staticmethod
+    def check_one_color(frame):
+        gray_image = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        avg_color_per_row = np.average(gray_image, axis=0)
+        avg_color = np.average(avg_color_per_row, axis=0)
+        if avg_color > 200:
+            return False
+        else:
+            return True
+
+    @staticmethod
+    def open_compare_video(video_name):
+        global_path_to_vide = os.getcwd()
+        global_path_to_vide = f"{global_path_to_vide[:global_path_to_vide.rfind('/')]}/{video_name}"
+        return global_path_to_vide
+
+    @staticmethod
+    def find_index(list1, list2):
+        for i in range(len(list1)):
+            if list1[i] == list2:
+                return i
+
+    def find_timecode_same_frames(self, time_list):
+        timing_list = []
+        end_same = []
+        while True:
+            start_same = time_list[0]
+            for i in range(1, len(time_list)):
+                compare_time_list = self.count_time(i, time_list[i-1])
+                if time_list[i] == compare_time_list:
+                    end_same = compare_time_list
+            else:
+                timing_list.append([start_same, end_same])
+                time_list = time_list[self.find_index(time_list, end_same):]
+                end_same = []
+                continue
+
+    def compare_video(self, video_name):
+        """
+        First compare to find same frames
+        :param video_name: video name to find all same
+        :return: list of same frames
+        """
+        self.folder_frames()
+        same_frames_time = []
+        time_list_orig = [0, 0, 0, 0]
+        frames_counter_orig = 1
+        while True:
+            ret_orig, frame_orig = self.video.read()
+            time_list_orig = self.count_time(frames_counter_orig, time_list_orig)
+            if ret_orig:
+                video_compare = cv2.VideoCapture(self.open_compare_video(video_name))
+                while True:
+                    ret_compare, frame_compare = video_compare.read()
+                    if ret_compare:
+                        difference = cv2.absdiff(frame_orig, frame_compare)
+                        if np.nonzero(difference):
+                            cv2.imwrite(
+                                self.image_name(time_list_orig),
+                                frame_orig
+                            )
+                            same_frames_time.append(time_list_orig)
+                            break
+                    else:
+                        break
+            else:
+                break
+            frames_counter_orig += 1
+        return same_frames_time
+
 
 start = time()
-path = '91_Days_[05]_[AniLibria_TV]_[HDTV-Rip_720p].mkv'
+path = 'test.mp4'
 video = Video_redactor(name=path)
 
+video_c = 'test.mp4'
+video.find_timecode_same_frames([[0, 4, 10, 0],
+                                [0, 4, 10, 0.033],
+                                [0, 4, 10, 0.066],
+                                [0, 4, 10, 0.099],
+                                [0, 4, 10, 0.132],
+                                [0, 4, 10, 0.165],
+                                [0, 4, 10, 0.198],
+                                [0, 4, 10, 0.231],
+                                [0, 4, 10, 0.264],
+                                [0, 4, 10, 0.297],
+                                [0, 4, 10, 0.33],
+                                [0, 4, 10, 0.363],
+                                [0, 4, 10, 0.396],
+                                [0, 4, 10, 0.429],
+                                [0, 4, 10, 0.462],
+                                [0, 4, 10, 0.495],
+                                [0, 4, 10, 0.528],
+                        [0, 4, 10, 0.561]])
 end: float = time() - start
 print(end)
