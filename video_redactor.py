aaa727a6409ebdb5fc7c07e09cf388dfa6bf4719
Leonid Tsarov <tsarov13leo@gmail.com>
2023-03-14 21:21:24 +0100

    v0.0.13_delete some not needed parts

--- video_redactor.py
+++ video_redactor.py
@@ -1,6 +1,7 @@
 import os
 from time import time
 import cv2
+from multiprocessing import Pool
 
 
 class Video_redactor:
@@ -25,35 +26,10 @@ class Video_redactor:
         """
         self.video = cv2.VideoCapture(self.name)
         self.name = self.name[:self.name.rfind('.')]
-        self.fps = self.video.get(cv2.CAP_PROP_FPS)
+        self.fps = round(self.video.get(cv2.CAP_PROP_FPS))
         self.fpms = round(1 / self.fps, 3)
         self.resolution = int(self.video.get(cv2.CAP_PROP_FRAME_HEIGHT))
         self.total_frames = int(self.video.get(cv2.CAP_PROP_FRAME_COUNT))
-
-    @staticmethod
-    def equal_to_60(num1, num2):
-        """
-        Check milliseconds or seconds, or minutes for +1 second, minute or hour
-        """
-        if num1 >= 60:
-            num1 = 0
-            num2 += 1
-        return num1, num2
-
-    def count_time(self, frame, time_list):
-        """
-        Video duration 24:30 round 8 minutes with cpu
-        Split video file to frames in folder with name of videofile
-        """
-        hours, minutes, seconds, milliseconds = time_list[0], time_list[1], time_list[2], time_list[3]
-        milliseconds += self.fpms
-        if frame % self.fps == 0:
-            milliseconds = 0
-            seconds += 1
-        seconds, minutes = self.equal_to_60(seconds, minutes)
-        minutes, hours = self.equal_to_60(minutes, hours)
-        time_list = [int(hours), int(minutes), int(seconds), round(milliseconds, 3)]
-        return time_list
 
     def folder_frames(self):
         """
@@ -68,35 +44,6 @@ class Video_redactor:
                 os.chdir(folder_path)
         except OSError:
             print(f"You don't have permission to change or create")
-
-    def image_name(self, time_list):
-        """
-        Make name for image
-        :param time_list: list with time code of frame
-        :return: self.name_HH:MM:SS:MSS
-        """
-        name = f'{self.name}'\
-               f'_{str(time_list[0]).zfill(2)}'\
-               f':{str(time_list[1]).zfill(2)}'\
-               f':{str(time_list[2]).zfill(2)}'\
-               f':{str(time_list[3])[2:].zfill(3)}.jpg'
-        return name
-
-    @staticmethod
-    def timing_name(start, end):
-        """
-        return start end round milliseconds
-        :param start:
-        :param end:
-        :return:
-        """
-        timing_start = f'{str(start[0]).zfill(2)}.' \
-                       f'{str(start[1]).zfill(2)}.' \
-                       f'{str(start[2]).zfill(2)}'
-        timing_end = f'{str(end[0]).zfill(2)}.' \
-                     f'{str(end[1]).zfill(2)}.' \
-                     f'{str(end[2]).zfill(2)}'
-        return timing_start, timing_end
 
     def split_video_to_frames(self):
         """
@@ -118,43 +65,32 @@ class Video_redactor:
                 break
 
     @staticmethod
-    def difference_gray_image(frame_orig, frame_compare):
-        """
-        Compare two frames with treshold 0.99
-        :param frame_orig: original frame
-        :param frame_compare: compare frame
-        :return: True or False
-        """
-        treshold = 0.99
-        gray_orig = cv2.cvtColor(frame_orig, cv2.COLOR_BGR2GRAY)
-        gray_compare = cv2.cvtColor(frame_compare, cv2.COLOR_BGR2GRAY)
-        difference = cv2.matchTemplate(gray_orig, gray_compare, cv2.TM_CCOEFF_NORMED)
-        min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(difference)
-        if max_val >= treshold:
-            return True
-        else:
-            return False
-
-    @staticmethod
     def open_compare_video(video_name):
         """
         Open vidio for first compare by global path
         :param video_name: video name
         :return:
         """
-        global_path_to_vide = os.getcwd()
-        global_path_to_vide = f"{global_path_to_vide[:global_path_to_vide.rfind('/')]}/{video_name}"
-        return global_path_to_vide
+        global_path_to_video = os.getcwd()
+        global_path_to_video = f"{global_path_to_video[:global_path_to_video.rfind('/')]}/{video_name}"
+        return global_path_to_video
+
+    def frame_count_to_time(self, frame):
+        hours, minutes = int(), int()
+        seconds = frame / self.fps
+        print(seconds, frame, self.fps)
+        minutes, seconds = self.equal_to_60(minutes, seconds)
+        hours, minutes = self.equal_to_60(hours, minutes)
+        frame_time = [hours, minutes, round(seconds, 3)]
+        print(frame_time, frame)
+        return frame_time
 
     @staticmethod
-    def sum_time(time_list):
-        """
-        Claculate all time in milliseconds
-        :param time_list: [HH, MM, SS, MMs]
-        :return: total MMs
-        """
-        time_sum = time_list[0] * 180 + time_list[1] * 60 + time_list[2] + time_list[3]
-        return time_sum
+    def equal_to_60(number1, number2):
+        if number2 >= 60:
+            number1 = int(number2 // 60)
+            number2 = number2 - (number1 * 60)
+        return number1, number2
 
     def find_timecode_same_frames(self, time_list):
         """
@@ -194,37 +130,49 @@ class Video_redactor:
             timing_start, timing_end = self.timing_name(start, end)
         print(timing_start, timing_end)
 
-    def compare_video(self, video_name, time_orig, time_compare):
+    @staticmethod
+    def difference_gray_image(frame_orig, frame_compare):
+        """
+        Compare two frames with treshold 0.99
+        :param frame_orig: original frame
+        :param frame_compare: compare frame
+        :return: True or False
+        """
+        treshold = 0.2
+        gray_orig = cv2.cvtColor(frame_orig, cv2.COLOR_BGR2GRAY)
+        gray_compare = cv2.cvtColor(frame_compare, cv2.COLOR_BGR2GRAY)
+        difference = cv2.matchTemplate(gray_orig, gray_compare, cv2.TM_CCOEFF_NORMED)
+        min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(difference)
+        if max_val >= treshold:
+            return True
+        else:
+            return False
+
+    def compare_videos(self, video_name, time_boards):
         """
         First compare to find same frames, and make folder with name of second seria
-        :param time_compare: list minute and second start and end at original video
-        :param time_orig: list minute and second start and end at video compare
+        :param time_boards: list start and end of orig and compare
         :param video_name: name of compare video
         :return: list of same frames orig video and second of compared
         """
         self.folder_frames()
-        start_time_orig, end_time_orig = time_orig[0], time_orig[1]
-        start_time_compare, end_time_compare = time_compare[0], time_compare[1]
         video_compare = cv2.VideoCapture(self.open_compare_video(video_name))
         same_frames_time_orig, same_frames_time_compare = list(), list()
-        time_list_orig, time_list_compare = [0, 0, 0, 0], [0, 0, 0, 0]
-        frames_counter_orig, frames_counter_compare = int(), int()
-        frames_flag = 1
+        frames_counter_orig, frames_counter_compare = -1, -1
+        frames_flag = int()
         skip_opening = False
 
         while True:
             ret_orig, frame_orig = self.video.read()
             if ret_orig:
-                time_list_orig = self.count_time(frames_counter_orig, time_list_orig)
                 frames_counter_orig += 1
                 # Break when time same to end time
-                if self.sum_time(time_list_orig) >= end_time_orig:
+                if frames_counter_orig >= time_boards[1]:
                     break
                 # Skip while time be at start mark
-                if self.sum_time(time_list_orig) >= start_time_orig:
+                elif frames_counter_orig >= time_boards[0]:
                     # If last frame was same, skip opening from the beginning
                     if not skip_opening:
-                        time_list_compare = [0, 0, 0, 0]
                         frames_counter_compare = int()
                         video_compare = cv2.VideoCapture(self.open_compare_video(video_name))
                     while True:
@@ -232,41 +180,35 @@ class Video_redactor:
                         # While video opening
                         if ret_compare:
                             frames_counter_compare += 1
-                            time_list_compare = self.count_time(frames_counter_compare, time_list_compare)
                             # Break when time same to end time
-                            if self.sum_time(time_list_compare) >= end_time_compare:
+                            if frames_counter_compare >= time_boards[3]:
                                 break
                             # Skip while time be at start mark
-                            if self.sum_time(time_list_compare) >= start_time_compare:
+                            if frames_counter_compare >= time_boards[2]:
                                 # When once same part was found, to don't start check from the beginning
                                 if frames_counter_compare >= frames_flag:
                                     # Check that frames same and put flag of same frame and flag to skip reopening
                                     if self.difference_gray_image(frame_orig, frame_compare):
-                                        cv2.imwrite(
-                                            self.image_name(time_list_orig),
-                                            frame_orig
-                                        )
-                                        same_frames_time_orig.append(time_list_orig)
-                                        same_frames_time_compare.append(time_list_compare)
+                                        frame_name = self.frame_count_to_time(frames_counter_orig)
+                                        same_frames_time_orig.append(frames_counter_orig)
+                                        same_frames_time_compare.append(frames_counter_compare)
                                         frames_flag = frames_counter_compare
                                         skip_opening = True
+                                        cv2.imwrite(
+                                            f'{str(frame_name[0]).zfill(2)}:'
+                                            f'{str(frame_name[1]).zfill(2)}:'
+                                            f'{str(frame_name[2]).zfill(5)}'
+                                            f'.jpg',
+                                            frame_orig
+                                        )
                                         break
-                                    else:
-                                        skip_opening = False
-                                        continue
                                 else:
-                                    continue
-                            else:
-                                continue
+                                    skip_opening = False
                         else:
                             break
-                else:
-                    continue
             else:
                 break
-        return same_frames_time_orig, same_frames_time_compare
-        # self.find_timecode_same_frames(same_frames_time_orig)
-        # self.find_timecode_same_frames(same_frames_time_compare)
+        print(same_frames_time_orig, same_frames_time_compare)
 
     def multiprocessing_cpu(self, cores_count, video_name):
         """
@@ -276,28 +218,79 @@ class Video_redactor:
         :return: time to slice both videos
         """
         orig_same_list, compare_same_list = list(), list()
-        video = cv2.VideoCapture.self.open_compare_video(video_name)
+        video = cv2.VideoCapture(video_name)
         total_frames = int(video.get(cv2.CAP_PROP_FRAME_COUNT))
         fps = video.get(cv2.CAP_PROP_FPS)
-        big_part_orig = self.total_frames / self.fps
-        big_part_compare = total_frames / fps
+        big_part_orig = (self.total_frames / self.fps) / 3
+        big_part_compare = (total_frames / fps) / 3
         small_part_orig = big_part_orig / cores_count
-        small_part_compare = big_part_compare / cores_count
+        for parts in range(3):
+            start_compare = (parts * big_part_compare)
+            end_compare = start_compare + big_part_compare
+            for piece in range(cores_count):
+                start_orig = (parts * big_part_orig) + (piece * small_part_orig)
+                end_orig = start_orig + small_part_orig
+                boards_list = [start_orig, end_orig, start_compare, end_compare]
+        print(orig_same_list, compare_same_list, sep='\n')
+
+    def compare_frames_Pool(self, frame_orig, frame_compare):
+        threshold = 0.95
+        gray_orig = cv2.cvtColor(frame_orig, cv2.COLOR_BGR2GRAY)
+        gray_compare = cv2.cvtColor(frame_compare, cv2.COLOR_BGR2GRAY)
+        h, w = gray_orig.shape
+        result = cv2.matchTemplate(gray_orig, gray_compare, cv2.TM_CCOEFF_NORMED)
+        min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(result)
+        if max_val >= threshold:
+            return True
+        else:
+            return False
+
+    def comapre_videos_Pool(self, video_name, boards):
+        self.folder_frames()
+        video_compare = cv2.VideoCapture(self.open_compare_video(video_name))
+        same_frames_orig, same_frames_compare = list(), list()
+        frames_counter_orig, frames_counter_compare = int(), int()
+        frames_flag = 1
+        skip_opening = False
+        with Pool() as pool:
+            while True:
+                ret_orig, frame_orig = self.video.read()
+                if ret_orig:
+                    if frames_counter_orig >= boards[0]:
+                        break
+                    elif frames_counter_orig >= boards[1]:
+                        if not skip_opening:
+                            frames_counter_compare = int()
+                            video_compare = cv2.VideoCapture(self.open_compare_video(video_name))
+                        while True:
+                            ret_compare, frame_compare = video_compare.read()
+                            if ret_compare:
+                                if frames_counter_compare >= boards[2]:
+                                    break
+                                elif frames_counter_compare >= boards[3] or frames_counter_compare <= frames_flag:
+                                    result = pool.apply_async(self.compare_frames_Pool, (frame_orig, frame_compare))
+                                    if result:
+                                        cv2.imwrite(frames_counter_orig, frame_orig)
+                                        same_frames_orig.append(frames_counter_orig)
+                                        same_frames_compare.append(frames_counter_compare)
+                                        frames_flag = frames_counter_compare
+                                frames_counter_compare += 1
+                            else:
+                                break
+                    frames_counter_orig += 1
+                else:
+                    break
+            print(same_frames_orig, same_frames_compare)
 
 
 start_time = time()
-
-
 path = '91_Days_[04]_[AniLibria_TV]_[HDTV-Rip_720p].mkv'
-video = Video_redactor(name=path)
-
+video0 = Video_redactor(name=path)
 video_c = '91_Days_[05]_[AniLibria_TV]_[HDTV-Rip_720p].mkv'
 
+time_listt = [120, 122, 2 * 60, 4 * 60]
+boards = [2880, 2885, 2880, 5760]
 
-print(video.fps)
-
-time_list = [120, 4 * 60, 2 * 60 + 35, 3 * 60]
-
-video.compare_video(video_c, time_list)
+video0.compare_videos(video_c, boards)
 
 print(time() - start_time)
